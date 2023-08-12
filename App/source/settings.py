7fd684f58cc7ede165e48991097f7e347b0588f9
Koech01 <01koech@gmail.com>
2023-08-12 20:54:53 +0300

    cleaning the settings py

--- App/source/settings.py
+++ App/source/settings.py
@@ -109,8 +109,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #For Sending Email with SMTP
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER     = "2004668@students.kcau.ac.ke"
-EMAIL_HOST_PASSWORD = 'sflbhfryugbowywe'
+EMAIL_HOST_USER     = "" #Your Email
+EMAIL_HOST_PASSWORD = '' #Your Password
 EMAIL_USE_TLS       = True
 EMAIL_PORT          = 587
 
